f4400e52357929ba00f4134dd773d94a11374111
gwuiinkim <gwuiinkim@gmail.com>
2019-08-10 22:45:46 +0900

    typescript state setting using hooks..

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC, useState } from "react";
 
-const App: React.FC = () => {
+interface IState {
+  initialValue: number;
+}
+
+const App: FC = () => {
+  const [clicks, setClicks] = useState(0);
+  const [value, setValue] = useState<Array<number>>([]);
+  console.log(value);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Clicks: {clicks}</p>
+      <button onClick={() => setClicks(clicks + 1)}>+</button>
+      <button onClick={() => setClicks(clicks - 1)}>-</button>
     </div>
   );
-}
+};
 
 export default App;
